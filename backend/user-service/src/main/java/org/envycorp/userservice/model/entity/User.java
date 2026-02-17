b5d2768bbad6864dfe2d3191c635f26870aa201b
Vladyslav <eddiedipper4228@gmail.com>
2026-02-17 23:40:10 +0100

    Added Lombok @Data annotation to User entity for automatic getter/setter generation

--- backend/user-service/src/main/java/org/envycorp/userservice/model/entity/User.java
+++ backend/user-service/src/main/java/org/envycorp/userservice/model/entity/User.java
@@ -2,11 +2,13 @@ package org.envycorp.userservice.model.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
