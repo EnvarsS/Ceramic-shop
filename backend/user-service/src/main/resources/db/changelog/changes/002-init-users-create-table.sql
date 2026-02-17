4bf0080fc30dbb404e38d0dba674339ce87f08e8
Vladyslav <eddiedipper4228@gmail.com>
2026-02-17 17:23:53 +0100

    fixed error

--- backend/user-service/src/main/resources/db/changelog/changes/002-init-users-create-table.sql
+++ backend/user-service/src/main/resources/db/changelog/changes/002-init-users-create-table.sql
@@ -5,5 +5,5 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role_id BIGINT REFERENCES roles(id) NOT NULL,
+    role_id BIGINT REFERENCES roles(id) NOT NULL
 );
