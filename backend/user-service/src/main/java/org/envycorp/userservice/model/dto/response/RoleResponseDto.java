d13aac4a488cf9dc2f07f3417dff3fd4964c20a6
Vladyslav <eddiedipper4228@gmail.com>
2026-02-18 01:26:52 +0100

    Final changes

--- backend/user-service/src/main/java/org/envycorp/userservice/model/dto/response/RoleResponseDto.java
+++ backend/user-service/src/main/java/org/envycorp/userservice/model/dto/response/RoleResponseDto.java
@@ -2,8 +2,10 @@ package org.envycorp.userservice.model.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class RoleResponseDto {
     private Long id;
