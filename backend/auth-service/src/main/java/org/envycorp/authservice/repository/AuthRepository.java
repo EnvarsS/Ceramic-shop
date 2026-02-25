84b1e72e6f3b93fe678b5356bc5777c567e719f3
Vladyslav <eddiedipper4228@gmail.com>
2026-02-25 18:50:49 +0100

    Refactor repository imports to use entity package for UserAuth and Role

--- backend/auth-service/src/main/java/org/envycorp/authservice/repository/AuthRepository.java
+++ backend/auth-service/src/main/java/org/envycorp/authservice/repository/AuthRepository.java
@@ -1,6 +1,6 @@
 package org.envycorp.authservice.repository;
 
-import org.envycorp.authservice.model.UserAuth;
+import org.envycorp.authservice.model.entity.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthRepository extends JpaRepository<UserAuth, Long> {
--- backend/auth-service/src/main/java/org/envycorp/authservice/repository/RoleRepository.java
+++ backend/auth-service/src/main/java/org/envycorp/authservice/repository/RoleRepository.java
@@ -1,6 +1,6 @@
 package org.envycorp.authservice.repository;
 
-import org.envycorp.authservice.model.Role;
+import org.envycorp.authservice.model.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role,Long> {
