baff92de5263c6397500ea13145cb25e9567d5b7
Vladyslav <eddiedipper4228@gmail.com>
2026-02-26 19:45:41 +0100

    Implement authentication endpoints and global exception handling

---
+++ backend/auth-service/src/main/java/org/envycorp/authservice/controller/ServiceExceptionHandler.java
@@ -0,0 +1,37 @@
+package org.envycorp.authservice.controller;
+
+import org.envycorp.authservice.exception.IncorrectPasswordException;
+import org.envycorp.authservice.exception.UsernameIsAlreadyTaken;
+import org.envycorp.authservice.exception.UsernameNotExists;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ServiceExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler({
+            IncorrectPasswordException.class,
+            UsernameIsAlreadyTaken.class,
+            UsernameNotExists.class
+    })
+    public ResponseEntity<Map<String, String>> handleCustomExceptions(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+}
