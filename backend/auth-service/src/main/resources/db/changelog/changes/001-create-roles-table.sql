f8a7da7b728471afe787f1671781277d672953cc
Vladyslav <eddiedipper4228@gmail.com>
2026-02-26 19:40:15 +0100

    Fix SQL syntax and update password hashes in initialization scripts for roles and auth_users

--- backend/auth-service/src/main/resources/db/changelog/changes/001-create-roles-table.sql
+++ backend/auth-service/src/main/resources/db/changelog/changes/001-create-roles-table.sql
@@ -1,7 +1,7 @@
 --Liquibase formatted SQL
 -- changeset envars:001
 
-CREATE ROLES(
-       id BIGSERIAl PRIMARY KEY,
+CREATE TABLE roles(
+       id BIGINT AUTO_INCREMENT PRIMARY KEY,
        name VARCHAR(50) NOT NULL UNIQUE
 )
