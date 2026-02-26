4e74740db043e976091d2ece11751f00d0f48429
Vladyslav <eddiedipper4228@gmail.com>
2026-02-26 19:45:50 +0100

    Add user registration and login methods with password hashing in AuthService

--- backend/auth-service/src/main/java/org/envycorp/authservice/repository/AuthRepository.java
+++ backend/auth-service/src/main/java/org/envycorp/authservice/repository/AuthRepository.java
@@ -4,4 +4,7 @@ import org.envycorp.authservice.model.entity.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthRepository extends JpaRepository<UserAuth, Long> {
+    boolean existsUserAuthByUsername(String username);
+
+    UserAuth findByUsername(String username);
 }
