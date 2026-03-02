81ec619f49ea8a8b59877520a79dcccdb0318038
Vladyslav <eddiedipper4228@gmail.com>
2026-03-02 19:00:24 +0100

    New DTO for separated login and registration logic

---
+++ backend/auth-service/src/main/java/org/envycorp/authservice/model/dto/UserRegisterRequestDto.java
@@ -0,0 +1,20 @@
+package org.envycorp.authservice.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegisterRequestDto {
+    @NotBlank
+    private String username;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+}
