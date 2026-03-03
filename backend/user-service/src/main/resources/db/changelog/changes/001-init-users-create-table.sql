2644985bfc92251dae23134cf701394c927d8356
Vladyslav <eddiedipper4228@gmail.com>
2026-03-03 19:19:54 +0100

    Changed DB structure and init data for matching new user service goals

--- backend/user-service/src/main/resources/db/changelog/changes/001-init-users-create-table.sql
+++ backend/user-service/src/main/resources/db/changelog/changes/001-init-users-create-table.sql
@@ -1,10 +1,9 @@
 -- Liquibase formatted SQL
 -- changeset envars:001
 CREATE TABLE users(
-    id         BIGSERIAL PRIMARY KEY,
+    id         BIGINT PRIMARY KEY,
     email      VARCHAR(255) NOT NULL UNIQUE,
     username   VARCHAR(255) NOT NULL UNIQUE,
-    auth_id    BIGINT       NOT NULL,
     address    VARCHAR(255),
     birth_date DATE,
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
