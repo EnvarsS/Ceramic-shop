53c8d5365a4869157e8fea68abe0b89c69a5082d
Vladyslav <eddiedipper4228@gmail.com>
2026-03-03 19:20:53 +0100

    Changed structure

--- backend/user-service/src/main/java/org/envycorp/userservice/model/dto/response/UserResponseDto.java
+++ backend/user-service/src/main/java/org/envycorp/userservice/model/dto/response/UserResponseDto.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class UserResponseDto {
     private Long id;
-    private String email;
-    private String username;
+    private String address;
+    private LocalDate birthDate;
+    private LocalDateTime createdAt;
 }
