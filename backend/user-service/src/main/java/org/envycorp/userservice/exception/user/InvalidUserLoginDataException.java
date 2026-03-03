aefd74b4f1d832ee5ebd20ea3a3a0db49053555f
Vladyslav <eddiedipper4228@gmail.com>
2026-03-03 19:19:05 +0100

    Deleted unused files

--- backend/user-service/src/main/java/org/envycorp/userservice/exception/user/InvalidUserLoginDataException.java
+++
@@ -1,7 +0,0 @@
-package org.envycorp.userservice.exception.user;
-
-public class InvalidUserLoginDataException extends RuntimeException {
-    public InvalidUserLoginDataException(String message) {
-        super(message);
-    }
-}
