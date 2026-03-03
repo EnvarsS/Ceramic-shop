c6281cd082669f56d0fc8e22e005be003a4a9401
Vladyslav <eddiedipper4228@gmail.com>
2026-03-03 19:21:16 +0100

    Created security part of service with authentication based on headers

---
+++ backend/user-service/src/main/java/org/envycorp/userservice/filter/HeaderAuthFilter.java
@@ -0,0 +1,33 @@
+package org.envycorp.userservice.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class HeaderAuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String username = request.getHeader("X-User-Username");
+        String role = request.getHeader("X-User-Role");
+
+        if(username != null || role != null){
+            UsernamePasswordAuthenticationToken token
+                    = new UsernamePasswordAuthenticationToken(username,null, List.of(new SimpleGrantedAuthority(role)));
+
+            SecurityContextHolder.getContext().setAuthentication(token);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
+
