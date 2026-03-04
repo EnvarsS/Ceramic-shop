b48d5e6c994b1fb8b0ce1adfd10db90ca2965264
Vladyslav <eddiedipper4228@gmail.com>
2026-03-04 14:33:14 +0100

    Changed the name of exception

--- backend/auth-service/src/main/java/org/envycorp/authservice/exception/NoPermissionError.java
+++
@@ -1,7 +0,0 @@
-package org.envycorp.authservice.exception;
-
-public class NoPermissionError extends RuntimeException {
-    public NoPermissionError(String message) {
-        super(message);
-    }
-}
