5a0c066c21c0a23b48aa132ed5e1c74bffd73564
Vladyslav <eddiedipper4228@gmail.com>
2026-03-05 18:25:41 +0100

    Created model class

---
+++ backend/product-service/src/main/java/org/envycorp/productservice/model/entity/Product.java
@@ -0,0 +1,27 @@
+package org.envycorp.productservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+}
