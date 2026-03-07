b66006a2baadeca1780e606fe53dd59c7452b205
Vladyslav <eddiedipper4228@gmail.com>
2026-03-07 16:21:15 +0100

    Created basic response dto

---
+++ backend/product-service/src/main/java/org/envycorp/productservice/model/dto/response/ProductResponseDto.java
@@ -0,0 +1,18 @@
+package org.envycorp.productservice.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponseDto {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer quantity;
+}
