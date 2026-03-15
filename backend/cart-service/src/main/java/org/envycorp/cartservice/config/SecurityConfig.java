43dd205ca5e99188eba019699f589e3232238cac
Vladyslav <eddiedipper4228@gmail.com>
2026-03-15 18:35:25 +0100

    Changed security config

--- backend/cart-service/src/main/java/org/envycorp/cartservice/config/SecurityConfig.java
+++ backend/cart-service/src/main/java/org/envycorp/cartservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.envycorp.cartservice.filter.HeaderAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,11 +16,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login").permitAll()
-                        .requestMatchers("/auth/register").permitAll()
-                        .requestMatchers("/auth/users/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(headerAuthFilter, UsernamePasswordAuthenticationFilter.class);
