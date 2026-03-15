3dd63a2dfed77d6735ab0a1e6678d0d9e89c40a0
Vladyslav <eddiedipper4228@gmail.com>
2026-03-15 18:30:30 +0100

    Changed the structure of entities to match business logic

--- backend/cart-service/src/main/java/org/envycorp/cartservice/model/entity/Cart.java
+++ backend/cart-service/src/main/java/org/envycorp/cartservice/model/entity/Cart.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table
 @AllArgsConstructor
@@ -14,4 +16,10 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long customerId;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> cartItems;
 }
