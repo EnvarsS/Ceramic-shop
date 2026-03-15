5daacf2c8a05f02d652cc8a45e97a69b96a598c1
Vladyslav <eddiedipper4228@gmail.com>
2026-03-15 19:23:49 +0100

    Changed implemented interface

--- backend/cart-service/src/main/java/org/envycorp/cartservice/repository/CartRepository.java
+++ backend/cart-service/src/main/java/org/envycorp/cartservice/repository/CartRepository.java
@@ -1,14 +1,14 @@
 package org.envycorp.cartservice.repository;
 
 import org.envycorp.cartservice.model.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CartRepository extends CrudRepository<Cart,Long> {
+public interface CartRepository extends JpaRepository<Cart,Long> {
 
     Optional<Cart> findCartByCustomerIdLazy(Long customerId);
 
