9658d7e026f596e69ce2883fc7cbe71425d0e901
Vladyslav <eddiedipper4228@gmail.com>
2026-03-15 18:36:21 +0100

    Changed the structure of Repository to add two methods for lazy and eager cart initializing(using join fetch)

--- backend/cart-service/src/main/java/org/envycorp/cartservice/repository/CartRepository.java
+++ backend/cart-service/src/main/java/org/envycorp/cartservice/repository/CartRepository.java
@@ -1,9 +1,17 @@
 package org.envycorp.cartservice.repository;
 
 import org.envycorp.cartservice.model.entity.Cart;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends CrudRepository<Cart,Long> {
+
+    Optional<Cart> findCartByCustomerIdLazy(Long customerId);
+
+    @Query("SELECT c FROM Cart c JOIN FETCH c.cartItems WHERE c.customerId = :customerId")
+    Optional<Cart> findCartByCustomerIdEager(Long customerId);
 }
