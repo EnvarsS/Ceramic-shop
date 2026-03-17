6de335568e6acd3c120c525c0e16f034fa989230
Vladyslav <eddiedipper4228@gmail.com>
2026-03-17 16:20:47 +0100

    Created basic test class and unit tests class

---
+++ backend/cart-service/src/test/java/org/envycorp/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.envycorp.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class CartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
