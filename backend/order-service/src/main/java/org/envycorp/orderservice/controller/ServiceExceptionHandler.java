2a7763f18df1c79831bc3f5595824e4c6d79e584
Vladyslav <eddiedipper4228@gmail.com>
2026-03-23 23:38:14 +0100

    Added new exception to service exception handler

--- backend/order-service/src/main/java/org/envycorp/orderservice/controller/ServiceExceptionHandler.java
+++ backend/order-service/src/main/java/org/envycorp/orderservice/controller/ServiceExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.envycorp.orderservice.controller;
 
+import org.envycorp.orderservice.exception.OrderIsNotExisted;
 import org.envycorp.orderservice.exception.WrongOrderIdException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ServiceExceptionHandler {
-    @ExceptionHandler(WrongOrderIdException.class)
-    public ResponseEntity<String> exceptionHandler(WrongOrderIdException exception) {
+    @ExceptionHandler({
+            WrongOrderIdException.class,
+            OrderIsNotExisted.class
+    })
+    public ResponseEntity<String> exceptionHandler(Exception exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
